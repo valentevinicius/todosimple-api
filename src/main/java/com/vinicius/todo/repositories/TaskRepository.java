89e81c1837a7a0caa945c8215677d3912a2b9a9d
unknown <viniciusvramirez0108@gmail.com>
2024-06-22 12:33:20 -0700

    Updated TaskRepository

--- src/main/java/com/vinicius/todo/repositories/TaskRepository.java
+++ src/main/java/com/vinicius/todo/repositories/TaskRepository.java
@@ -3,8 +3,6 @@ package com.vinicius.todo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vinicius.todo.models.Task;
@@ -12,11 +10,11 @@ import com.vinicius.todo.models.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     
-    // List<Task> findByUser_Id(Long id);
+    List<Task> findByUser_Id(Long id);
 
     // @Query(value = "SELECT t from Task t WHERE t.user.id = :user_id")
     // List<Task> findByUser_Id(@Param("user_id") Long user_id); // Função
 
-    @Query(value = "SELECT * FROM task t WHERE t.user_id = :user_id", nativeQuery = true)
-    List<Task> findByUser_Id(@Param("user_id") Long user_id);
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :user_id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("user_id") Long user_id);
 }
