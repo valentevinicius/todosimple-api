832c934cf4d2e4858c0c1c162c5e9f61807176e5
unknown <viniciusvramirez0108@gmail.com>
2024-06-22 14:15:20 -0700

    Refactor UserService

--- src/main/java/com/vinicius/todo/services/UserService.java
+++ src/main/java/com/vinicius/todo/services/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vinicius.todo.models.User;
-import com.vinicius.todo.repositories.TaskRepository;
 import com.vinicius.todo.repositories.UserRepository;
 
 @Service
@@ -15,9 +14,6 @@ public class UserService {
     
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
@@ -30,7 +26,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
