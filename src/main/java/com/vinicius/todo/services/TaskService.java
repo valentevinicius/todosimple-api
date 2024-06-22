fc80b49a3636a0255e375e9f959ed14a551dac21
unknown <viniciusvramirez0108@gmail.com>
2024-06-22 14:14:46 -0700

    Created TaskService

---
+++ src/main/java/com/vinicius/todo/services/TaskService.java
@@ -0,0 +1,54 @@
+package com.vinicius.todo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vinicius.todo.models.Task;
+import com.vinicius.todo.models.User;
+import com.vinicius.todo.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+            "Task not found!\nId: " + id +", Tipo: " + Task.class.getName()
+            )
+        );
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Not possible to delete, there are related entities");
+        }
+    }
+}
