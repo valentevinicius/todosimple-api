3af32b53c1a12fde7dd38990bd37dabedde56aa8
unknown <viniciusvramirez0108@gmail.com>
2024-06-23 16:04:07 -0700

    Created TaskController

--- src/main/java/com/vinicius/todo/controllers/TaskController.java
+++ src/main/java/com/vinicius/todo/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package com.vinicius.todo.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -17,48 +18,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.vinicius.todo.models.User;
-import com.vinicius.todo.models.User.CreateUser;
-import com.vinicius.todo.models.User.UpdateUser;
-import com.vinicius.todo.services.UserService;
+import com.vinicius.todo.models.Task;
+import com.vinicius.todo.services.TaskService;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
-@RestController //APIs RESTful
-@RequestMapping("/user")
+@RestController
+@RequestMapping("/task")
 @Validated
-public class UserController {
+public class TaskController {
     
-
     @Autowired
-    private UserService userService;
+    private TaskService taskService;
 
-    // localhost:8080/user/1 <-numero do usuário
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        User obj = this.userService.findById(id);
-        return ResponseEntity.ok().body(obj); //Retorna 200 OK com o body da requisição
+    public ResponseEntity<Task> findById(@PathVariable Long id) {
+        Task obj = this.taskService.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId) {
+        List<Task> objs = this.taskService.findAllByUserId(userId);
+        return ResponseEntity.ok().body(objs);
     }
 
     @PostMapping
-    @Validated(CreateUser.class)
-    public ResponseEntity<Void> create(@Valid @RequestBody User obj) {
-        this.userService.create(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()// acessar /user/
-            .path("/{id}").buildAndExpand(obj.getId()).toUri();// Acessar path /{id} e builda o objeto
-        return ResponseEntity.created(uri).build(); //(201) Created
+    @Validated
+    public ResponseEntity <Void> create (@Valid @RequestBody Task obj) {
+        this.taskService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @PutMapping("/{id}")
-    @Validated(UpdateUser.class)
-    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id) {
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody Task obj, @PathVariable Long id) {
         obj.setId(id);
-        this.userService.update(obj);
+        this.taskService.update(obj);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        this.userService.delete(id);
+        this.taskService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
 }
--- src/main/java/com/vinicius/todo/services/TaskService.java
+++ src/main/java/com/vinicius/todo/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.vinicius.todo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class TaskService {
     @Autowired
     private UserService userService;
 
+    public List<Task> findAllByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
+    }
+    
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException(
